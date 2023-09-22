3ce6130b3c726e95c139da2c0d07128279fe7fb2
Zachary Soliman <zachsoliman1@gmail.com>
2023-09-22 16:00:20 -0400

    header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 import Startgame from './components/Startgame';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Startgame />
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Dog or Donkey</h1>
+        </div>
+    )
+}
+
+export default Header;
